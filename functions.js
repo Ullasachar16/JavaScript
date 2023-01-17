3b006afd9cfccfa4b7871fb01d5427c2a3974e2e
Ullasachar16 <ullasachar16@gmail.com>
2023-01-17 15:11:19 +0530

    named function

--- functions.js
+++ functions.js
@@ -6,7 +6,12 @@ function a(){
     console.log("a called");
 }
 //Function Expression
-var b=function (){
+// var b=function (){
+//     console.log("b called");
+// }
+// b();
+//Named Function
+var b=function xyz(){
     console.log("b called");
 }
 b();
