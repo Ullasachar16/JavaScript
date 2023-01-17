49be8c3744d3e0e9e8454768d1687aeea49e144f
Ullasachar16 <ullasachar16@gmail.com>
2023-01-17 14:42:48 +0530

    got to know how const works in shadowing

--- blockscope.js
+++ blockscope.js
@@ -1,11 +1,20 @@
-{
-    var a=10;
-    let b=20;
+// {
+//     var a=10;
+//     let b=20;
+//     const c=30;
+//     console.log(a);
+// console.log(b);
+// console.log(c);
+// }
+// console.log(a); //it shadows here the same value inside block as same result
+// console.log(b);
+// console.log(c);
+
+//shadowing in functions
+const c=100;
+function x(){
     const c=30;
-    console.log(a);
-console.log(b);
-console.log(c);
+    console.log(c);
 }
-console.log(a); //it shadows here the same value inside block as same result
-console.log(b);
 console.log(c);
+x();
