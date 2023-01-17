3c6d1acc7878cab012740be7f5dfc4360dbd3d89
Ullasachar16 <ullasachar16@gmail.com>
2023-01-17 14:38:11 +0530

    known shadowing

--- blockscope.js
+++ blockscope.js
@@ -2,4 +2,10 @@
     var a=10;
     let b=20;
     const c=30;
+    console.log(a);
+console.log(b);
+console.log(c);
 }
+console.log(a); //it shadows here the same value inside block as same result
+console.log(b);
+console.log(c);
