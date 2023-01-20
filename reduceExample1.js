a69f375c38480b6c5ba46d477ce8d8d474e5c370
Ullasachar16 <ullasachar16@gmail.com>
2023-01-20 15:08:53 +0530

    changes

--- reduceExample1.js
+++ reduceExample1.js
@@ -1,15 +1,15 @@
-const arr=[5,1,3,2,6];
+const array=[5,1,3,2,6];
 //find max
 //normal way
 
-function findMax(arr){
+function findMax(array){
     let max=0;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>max){
-            max=arr[i];
+    for(let i=0;i<=array.length;i++){
+        if(array[i]>max){
+            max=array[i];
         }
         return max;
     }
 }
-console.log(findMax(arr));
+console.log(findMax(array));
 
