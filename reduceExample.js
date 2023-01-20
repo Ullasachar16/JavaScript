7a899d79e5f222e2f21729c548847601e59386e1
Ullasachar16 <ullasachar16@gmail.com>
2023-01-20 15:02:41 +0530

    added another option

--- reduceExample.js
+++ reduceExample.js
@@ -9,3 +9,12 @@ function findSum(arr){
     return sum;
 }
 console.log(findSum(arr));
+
+//by using reduce[]
+
+const output=arr.reduce(function(acc,curr){
+    acc=acc+curr;
+    return acc;
+},0);
+
+console.log(output);
