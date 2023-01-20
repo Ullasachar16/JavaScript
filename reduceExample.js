a24bdeba929730b54f0fb2d200339bd3195afd00
Ullasachar16 <ullasachar16@gmail.com>
2023-01-20 15:01:14 +0530

    slight change

--- reduceExample.js
+++ reduceExample.js
@@ -3,7 +3,7 @@ const arr=[5,1,3,2,6];
 //normal wa
 function findSum(arr){
     let sum=0;
-    for(let i=1;i<arr.length;i++){
+    for(let i=0;i<arr.length;i++){
         sum=sum+arr[i];
     }
     return sum;
