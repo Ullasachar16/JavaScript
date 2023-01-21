db9e61795dfcffb8800bac5fb0f9f87537fe09c6
Ullasachar16 <ullasachar16@gmail.com>
2023-01-21 12:19:41 +0530

    some more clarity

--- call-apply-bind.js
+++ call-apply-bind.js
@@ -2,14 +2,14 @@ let name={
     firstname: "Akshay",
     lastname: "Saini",
 }
-let printFullName= function (hometown){
-    console.log(this.firstname+" "+this.lastname+ " from "+hometown);
+let printFullName= function (hometown,state){
+    console.log(this.firstname+" "+this.lastname+ " from "+hometown+ " , "+state);
 }
-printFullName.call(name,"Dehradun");
+printFullName.call(name,"Dehradun","Uttarakhand");
 
 let name1={
     firstname: "Sachin",
     lastname: "Tendulkar",
 }
 
-printFullName.call(name1,"Mumbai");
+printFullName.call(name1,"Mumbai","Maharashtra");
