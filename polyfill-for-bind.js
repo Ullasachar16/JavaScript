2d216bfa84b26fcb0742a0a9213c311d20a44cf7
Ullasachar16 <ullasachar16@gmail.com>
2023-01-21 12:46:14 +0530

    basic implementation of mybind

--- polyfill-for-bind.js
+++ polyfill-for-bind.js
@@ -7,3 +7,13 @@ let printName= function (){
 }
 let printMyName = printName.bind(name);
 printMyName();
+
+Function.prototype.mybind=function(...args){
+    let obj=this;
+    return function(){
+        obj.call(args[0]);
+    }
+}
+
+let printMyName1 = printName.mybind(name);
+printMyName1();
