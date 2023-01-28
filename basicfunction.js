f219fc0eb9893931c68b6f0a4e0bc7338aab983e
Ullasachar16 <ullasachar16@gmail.com>
2023-01-28 13:23:31 +0530

    getting to know functions

---
+++ basicfunction.js
@@ -0,0 +1,15 @@
+function getMilk(){
+    console.log("leavehouse");
+    console.log("move right");
+    console.log("moveUp");
+    console.log("move right");
+    console.log("enter store");
+    console.log("buy milk");
+    console.log("exit store");
+    console.log("move left");
+    console.log("move down");
+    console.log("move left");
+    console.log("enter house");
+}
+getMilk();
+
