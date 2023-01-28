8c3ef49c7cb5e66faaa2c9bd6a61b03d92d4bfb3
Ullasachar16 <ullasachar16@gmail.com>
2023-01-28 14:08:04 +0530

    changed input

--- functionadvanced2.js
+++ functionadvanced2.js
@@ -21,4 +21,4 @@ function calcChange(startingAmount, costPerBottle) {
     var change=startingAmount % costPerBottle;
     return change;
 }
-console.log("Hello master, here is your "+getMilk(10,3)+" change");
+console.log("Hello master, here is your "+getMilk(5,2.5)+" change");
