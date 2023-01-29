37b636a67a5fd0e7e484a689bb31398809cf9d7f
Ullasachar16 <ullasachar16@gmail.com>
2023-01-29 11:26:57 +0530

    done with functions

--- BMIfunction.js
+++ BMIfunction.js
@@ -2,6 +2,6 @@ function calculateBMI(weight, height) {
     var bmi=weight / Math.pow(height,2);
     return Math.round(bmi);
   }
-  var bmii=calculateBMI(65,1.8); 
-  console.log(bmii);
+  const bmi = calculateBMI(65,1.8);
+  console.log(bmi)
   
