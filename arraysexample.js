0a6f9718029c22183c5fd7bb234029ff2d3ca8e1
Ullasachar16 <ullasachar16@gmail.com>
2023-01-30 11:29:18 +0530

    slight changes

--- arraysexample.js
+++ arraysexample.js
@@ -1,10 +1,9 @@
 var guestList=["akshay","bhaskar","chaithanya","dhanush",
 "kruthika","sujay","suraj","ullas"];
 var name=prompt("Enter your name!!!");
-for(i=0;i<guestList.length;i++){
-    if(name[i]===guestList[i]){
-        alert("Welcome!!!");
-    }else{
-        alert("Sorry maybe next time");
-    }
+if(guestList.includes(name)){
+    alert("Welcome!!!");
+}else{
+    alert("Sorry next time");
 }
+
