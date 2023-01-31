bbe12d401a1dea8370a09a44caebf4392a4e6aaa
Ullasachar16 <ullasachar16@gmail.com>
2023-01-31 11:46:12 +0530

    wrote code

---
+++ 99bottlesofbearsong.js
@@ -0,0 +1,5 @@
+var i=99;
+while(i>=0){
+    console.log(i+" bottles of beer on the wall, "+i+" bottles of beer.\n"+
+    "Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
+}
