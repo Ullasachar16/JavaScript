29b8c8b9886aefbc4cf966a71e0f9bdfa39c22a7
Ullasachar16 <ullasachar16@gmail.com>
2023-01-31 12:05:21 +0530

    changed back to while loop

--- 99bottlesofbearsong.js
+++ 99bottlesofbearsong.js
@@ -1,13 +1,11 @@
-for(var i=99;i>=0;i--){
-    if(i===0){
-        console.log("No more bottles of beer on the wall, no more bottles of beer.\n"+"Go to the store and buys some more,"+i+"bottles of beer on the wall.");
-        break;
-    }
-    if(i===1){
-        console.log(i+" bottle of beer on the wall, no more bottles of beer.\n"+"Go to the store and buys some more,"+" no more bottles of beer on the wall.");
-        break;
-    }
-    console.log(i+" bottle of beer on the wall, "+i+" bottle of beer.\n"+
-    "Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
+while (numberOfBottles &gt!= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 }
-
