b4f46e31f43ad44999c809776ca8a60870c12f03
Ullasachar16 <ullasachar16@gmail.com>
2023-01-31 11:53:40 +0530

    done with challenge

--- 99bottlesofbearsong.js
+++ 99bottlesofbearsong.js
@@ -1,6 +1,13 @@
 var i=99;
 while(i>=0){
-    console.log(i+" bottles of beer on the wall, "+i+" bottles of beer.\n"+
+    console.log(i+" bottle of beer on the wall, "+i+" bottle of beer.\n"+
     "Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
     i--;
+    if(i===1){
+        console.log("No more bottles of beer on the wall, no more bottles of beer.\n"+"Go to the store and buys some more,"+" no more bottles of beer on the wall.") 
+    }
+    if(i===0){
+        console.log("No more bottles of beer on the wall, no more bottles of beer.\n"+"Go to the store and buys some more,"+i+"bottles of beer on the wall.") 
+     }
 }
+
