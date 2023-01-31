a3151a6590364c4e8bb0b2b7d7ae586ffac90484
Ullasachar16 <ullasachar16@gmail.com>
2023-01-31 12:15:41 +0530

    done with all possibilities

--- 99bottlesofbearsong.js
+++ 99bottlesofbearsong.js
@@ -1,8 +1,9 @@
 var numberOfBottles = 99;
-while (numberOfBottles >= 0) {
+while (numberOfBottles > 0) {
     var bottleWord = "bottle";
     if (numberOfBottles === 1) {
         bottleWord = "bottles";
+        console.log("no more " + bottleWord + " of beer on the wall.");
     } 
     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
     console.log(numberOfBottles + " " + bottleWord + " of beer,");
